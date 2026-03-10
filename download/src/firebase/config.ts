5de8cce76466ecec7af3eed525cf3ed25376714e
mineuzbcraft <mineuzbcraft@gmail.com>
2026-03-10 09:13:03 +0500

    Delete download directory

--- download/src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-1809717656-1494d",
-  "appId": "1:638617098628:web:942867a644fe182971a5d1",
-  "apiKey": "AIzaSyB_GmUHqib_xPSv99lNTpLozVm51OW2s-4",
-  "authDomain": "studio-1809717656-1494d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "638617098628"
-};
